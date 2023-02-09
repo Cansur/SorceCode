554409cab55b10a1707feb58b6ab0dd282a0a02a
Cansur <tjswo0682@gmail.com>
2023-02-10 00:06:37 +0900

    02_10

---
+++ test/test.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+f_path = 'C:/Users/tjswo/OneDrive/바탕 화면/이선재/SorceCode/test/'
+
+identity = {
+    'name' : ['Cansur', 'BY_poir'],
+    'money' : [1000, 10000],
+    'level' : [100, 300]
+}
+
