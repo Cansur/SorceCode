00a3b49310c95eb88db0222880447b223cdf213e
Cansur <tjswo0682@gmail.com>
2023-02-09 20:20:41 +0900

    02_09

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-# a = [0 for i in range(3)]
-# for i in range(3):
-#     a[i] = input()
-# if a[1] == "+":
-#     print(int(a[0]) + int(a[2]))
-# else:
-#     print(int(a[0]) * int(a[2]))
+import os
+import pandas as pd
+
+f_path = r'C:\Users\tjswo\OneDrive\바탕 화면\이선재\SorceCode\test.py'
+f_name = "data_tmp.csv"
+
+df3 = pd.DataFrame({'B': ['a', 'c', 'b', 'd'],
+                    'C': [3, 6, 9, 1],
+                    'D': ['b', 'b', 'e', 'a']})
+# f = open('hh.txt','w')
+# f.write(f_path)
+# f.close()
+# line = f.readline()
+# print(line)
+df3.to_csv(index=False)
