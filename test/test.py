1c269b1bab7fda772bf0ec8a6907d27ce5b75f56
Cansur <tjswo0682@gmail.com>
2023-02-13 21:55:06 +0900

    0213

--- test/test.py
+++ test/test.py
@@ -1,1 +1,8 @@
-
+from itertools import combinations
+var_list = [20,7,23,19,10,15,25,8,13]
+for i in combinations(var_list, 7):
+    if sum(i) == 100:
+        var_list = i
+        break
+var_list = list(var_list)
+print(*var_list, sep = '\n')
