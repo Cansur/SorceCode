8315161e161bdecbf924eef90234ae2547e9e9dc
Cansur <tjswo0682@gmail.com>
2023-02-13 21:59:09 +0900

    [2309] 브론즈1_일곱난쟁이

--- test/test.py
+++ test/test.py
@@ -1,8 +1,11 @@
+# combinations --> 모든합 구하는 함수
 from itertools import combinations
-var_list = [20,7,23,19,10,15,25,8,13]
+# var_list = [20,7,23,19,10,15,25,8,13]
+var_list = [int(input()) for _ in range(9)]
 for i in combinations(var_list, 7):
     if sum(i) == 100:
         var_list = i
         break
 var_list = list(var_list)
+var_list.sort()
 print(*var_list, sep = '\n')
