1e2ca5b32a662fdb3cb12ba78d00384e56983172
Cansur <tjswo0682@gmail.com>
2023-02-17 02:09:23 +0900

    [1110] 브론즈_더하기사이클

--- test/test.py
+++ test/test.py
@@ -1,18 +1,12 @@
-import math
-a, b = 24, 18
-# print(math.gcd(a, b)) 최대 공약수
-# print(math.lcm(a, b)) 최소 공배수
-
-# 최대 공약수
-for i in range(min(a,b), 0, -1):
-    if a%i == 0 and b%i == 0:
-        print(i)
-        # 최소 공배수
-        # print(a*b//i)
+n = int(input())
+count = 1
+var = n
+while True:
+    x = (var//10) + (var%10)
+    new = ((var%10)*10) + (x%10)
+    if n == new:
+        print(count)
         break
-
-# 최소 공배수
-for i in range(max(a, b), (a*b)+1):
-    if i%a == 0 and i%b == 0:
-        print(i)
-        break
+    else:
+        count += 1
+        var = new
