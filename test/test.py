e3746f108a2bdb5656840d73099697eeb75336e9
Cansur <tjswo0682@gmail.com>
2023-02-20 00:06:57 +0900

    [1712] 브론즈2_손익분기점

--- test/test.py
+++ test/test.py
@@ -1,18 +1,7 @@
-e_list = {
-    'ABC' : 3,
-    'DEF' : 4,
-    'GHI' : 5,
-    'JKL' : 6,
-    'MNO' : 7,
-    'PQRS' : 8,
-    'TUV' : 9,
-    'WXYZ' : 10
-}
-n = input()
-var = 0
-for k in n:     # n을 하나하나 쪼갠 문자 k 
-    for i in e_list:    # e_list안에 있는 요소를 i로 불러오기
-        for j in i:     # 요소안에 문자열을 문자로 쪼갠 문자 j
-            if k == j:
-                var += e_list[i]
-print(var)
+a,b,c = map(int, input().split())
+
+if b < c:
+    var = a//(c-b)
+    print(var + 1)
+else:
+    print(-1)
