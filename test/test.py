b1e2285d2aaf5f3b09e0ed3bac75982dfe00f535
Cansur <tjswo0682@gmail.com>
2023-02-19 19:28:47 +0900

    [5622] 브론즈2_다이얼

--- test/test.py
+++ test/test.py
@@ -1,12 +1,18 @@
-n = int(input())
-count = 1
-var = n
-while True:
-    x = (var//10) + (var%10)
-    new = ((var%10)*10) + (x%10)
-    if n == new:
-        print(count)
-        break
-    else:
-        count += 1
-        var = new
+e_list = {
+    'ABC' : 3,
+    'DEF' : 4,
+    'GHI' : 5,
+    'JKL' : 6,
+    'MNO' : 7,
+    'PQRS' : 8,
+    'TUV' : 9,
+    'WXYZ' : 10
+}
+n = input()
+var = 0
+for k in n:     # n을 하나하나 쪼갠 문자 k 
+    for i in e_list:    # e_list안에 있는 요소를 i로 불러오기
+        for j in i:     # 요소안에 문자열을 문자로 쪼갠 문자 j
+            if k == j:
+                var += e_list[i]
+print(var)
