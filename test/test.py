6d3683afbf20fdb9b227e08bee13bc144e17d3b7
Cansur <tjswo0682@gmail.com>
2023-02-21 14:36:15 +0900

    [10813] 브론즈2_공바꾸기

--- test/test.py
+++ test/test.py
@@ -1,7 +1,8 @@
 n, m = map(int, input().split())
-n_list = [0 for _ in range(n)]
-for p in range(m):
-    i, j, k = map(int, input().split())
-    for q in range(i-1, j):
-        n_list[q] = k
+n_list = [i for i in range(1, n+1)]
+for i in range(m):
+    i, j = map(int, input().split())
+    temp = n_list[j-1]
+    n_list[j-1] = n_list[i-1]
+    n_list[i-1] = temp
 print(*n_list)
