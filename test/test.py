0f6cef6a0b05ad66a7e59e8a0842f0bc3de2a461
Cansur <tjswo0682@gmail.com>
2023-02-21 15:11:36 +0900

    [10811] 브론즈2_바구니뒤집기

--- test/test.py
+++ test/test.py
@@ -1,8 +1,18 @@
 n, m = map(int, input().split())
+# n, m = 5,4
 n_list = [i for i in range(1, n+1)]
 for i in range(m):
+    temp = []
+    d = 0
     i, j = map(int, input().split())
-    temp = n_list[j-1]
-    n_list[j-1] = n_list[i-1]
-    n_list[i-1] = temp
+    # i, j = 1, 2
+    for k in range(i-1, j):
+        temp.append(n_list[k])
+    temp.reverse()
+    for k in range(i-1, j):
+        n_list[k] = temp[d]
+        d +=1
 print(*n_list)
+
+# 2 1 4 3 5
+# 3 4 1 2 5
