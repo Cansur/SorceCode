d44f45f4987d4592ccaa2c6cfed091ef678254cb
Cansur <tjswo0682@gmail.com>
2023-02-22 20:07:44 +0900

    [25206] 실버5_너의평점은

--- test/test.py
+++ test/test.py
@@ -1,9 +1,19 @@
-n = input()
-is_n = True
-for i in range(len(n)//2):
-    if n[i] != n[len(n)-i-1]:
-        is_n = False
-if is_n:
-    print(1)
-else:
-    print(0)
+data = {
+    'A+' : 4.5,
+    'A0' : 4.0,
+    'B+' : 3.5,
+    'B0' : 3.0,
+    'C+' : 2.5,
+    'C0' : 2.0,
+    'D+' : 1.5,
+    'D0' : 1.0,
+    'F' : 0.0
+}
+sum = 0; a_sum = 0
+for _ in range(20):
+    k, a, b = input().split()
+    a = float(a)
+    if b == 'P': continue
+    sum += a*data[b]
+    a_sum += a
+print('{:.6f}'.format(sum/a_sum))
