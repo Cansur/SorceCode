8b2537d34568a7751f77696445d18ad3480d44b0
Cansur <tjswo0682@gmail.com>
2023-02-22 19:30:00 +0900

    [10988] 브론즈2_팰린드롬

--- test/test.py
+++ test/test.py
@@ -1,18 +1,9 @@
-n, m = map(int, input().split())
-# n, m = 5,4
-n_list = [i for i in range(1, n+1)]
-for i in range(m):
-    temp = []
-    d = 0
-    i, j = map(int, input().split())
-    # i, j = 1, 2
-    for k in range(i-1, j):
-        temp.append(n_list[k])
-    temp.reverse()
-    for k in range(i-1, j):
-        n_list[k] = temp[d]
-        d +=1
-print(*n_list)
-
-# 2 1 4 3 5
-# 3 4 1 2 5
+n = input()
+is_n = True
+for i in range(len(n)//2):
+    if n[i] != n[len(n)-i-1]:
+        is_n = False
+if is_n:
+    print(1)
+else:
+    print(0)
