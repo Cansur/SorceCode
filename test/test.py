a1aeffe24dc4f12bd643e8654dc3d59d33d147b6
Cansur <tjswo0682@gmail.com>
2023-02-26 10:16:40 +0900

    [10816] 실버4

--- test/test.py
+++ test/test.py
@@ -1,9 +1,17 @@
-for _ in range(int(input())):    
-    h, w, n = map(int, input().split())
-    floor = n%h
-    room = (n//h)+1
-    if floor == 0: floor = h; room -= 1
-    if room < 10:
-        print(str(floor) + '0' + str(room))
+n = int(input())
+n_list = list(map(int, input().split()))
+m = int(input())
+m_list = list(map(int, input().split()))
+
+counts = {}
+for i in n_list:
+    if i in counts:
+        counts[i] += 1
     else:
-        print(str(floor) + str(room))
+        counts[i] = 1
+
+for i in m_list:
+    if counts.get(i) == None:
+        print('0', end = ' ')
+    else:
+        print(counts[i], end = ' ')
