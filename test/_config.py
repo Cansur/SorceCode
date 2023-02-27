0a1fcc20ba6dc719655544b5ace39074e283a18a
Cansur <tjswo0682@gmail.com>
2023-02-27 13:10:08 +0900

    [10866] 실버4_덱

--- test/_config.py
+++ test/_config.py
@@ -1,4 +1,37 @@
-word = input()
-alp = list(range(97,123))
-for i in alp:
-    print(word.find(chr(i)))
+from sys import stdin
+from collections import deque
+n = int(input())
+dq = deque()
+for i in range(n):
+    input = stdin.readline().split()
+    if input[0] == 'push_front':
+        dq.appendleft(input[1])
+    elif input[0] == 'push_back':
+        dq.append(input[1])
+    elif input[0] == 'pop_front':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.popleft())
+    elif input[0] == 'pop_back':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.pop())
+    elif input[0] == 'size':
+        print(len(dq))
+    elif input[0] == 'empty':
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif input[0] == 'front':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[0])
+    elif input[0] == 'back':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[-1])
