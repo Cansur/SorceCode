8e8dba60730073bf76856a1cd35991a8b10eac02
Cansur <tjswo0682@gmail.com>
2023-02-27 13:48:49 +0900

    [11650] 실버5_좌표정렬

--- test/test.py
+++ test/test.py
@@ -1,32 +1,8 @@
-import sys
-
-n = int(sys.stdin.readline())
-list = []
+from sys import stdin
+n = int(input())
+xy = []
+for _ in range(n):
+    xy.append(list(map(int, stdin.readline().split())))
+xy = sorted(xy)
 for i in range(n):
-    typenum = sys.stdin.readline().split()
-    
-    if typenum[0] == 'push':
-        list.append(typenum[1])
-    elif typenum[0] == 'pop':
-        if len(list) == 0:
-            print(-1)
-        else:
-            print(list.pop(0))
-    elif typenum[0] == 'size':
-        print(len(list))
-    elif typenum[0] == 'empty':
-        if len(list) == 0:
-            print(1)
-        else:
-            print(0)
-    elif typenum[0] == 'front':
-        if len(list) == 0:
-            print(-1)
-        else:
-            print(list[0])
-    elif typenum[0] == 'back':
-        if len(list) == 0:
-            print(-1)
-        else:
-            print(list[-1])
-    
+    print(*xy[i])
