a69d7ed3cad59d1591306f59166682264637aa45
Cansur <tjswo0682@gmail.com>
2023-02-28 14:11:42 +0900

    [11866] 실버5_요세푸스

--- test/test.py
+++ test/test.py
@@ -1,8 +1,13 @@
-from sys import stdin
-n = int(input())
-xy = []
-for _ in range(n):
-    xy.append(list(map(int, stdin.readline().split())))
-xy = sorted(xy)
+
+n, k = map(int, input().split())
+arr_n = [i for i in range(1, n+1)]
+arr_answer = []
+index_n = 0
 for i in range(n):
-    print(*xy[i])
+    index_n += k-1
+    if index_n >= len(arr_n):
+        index_n %= len(arr_n)
+    arr_answer.append(arr_n.pop(index_n))
+print('<', end = '')
+print(*arr_answer, sep= ', ', end = '')
+print('>', end = '')
