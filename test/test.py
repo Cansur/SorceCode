0f1e72064bf01f1f8a9167f92624a6564d9d32bb
Cansur <tjswo0682@gmail.com>
2023-02-28 15:18:03 +0900

    [2775] 브론즈1_부녀회장

--- test/test.py
+++ test/test.py
@@ -1,13 +1,17 @@
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    arr_inc = [i for i in range(1, n+1)]
+    arr_get = [0 for _ in range(n)]
+    for i in range(k-1):
+        for j in range(n):
+            for p in range(j+1):
+                arr_get[j] += arr_inc[p]
+        arr_inc = arr_get
+        arr_get = [0 for _ in range(n)]
+    print(sum(arr_inc))
 
-n, k = map(int, input().split())
-arr_n = [i for i in range(1, n+1)]
-arr_answer = []
-index_n = 0
-for i in range(n):
-    index_n += k-1
-    if index_n >= len(arr_n):
-        index_n %= len(arr_n)
-    arr_answer.append(arr_n.pop(index_n))
-print('<', end = '')
-print(*arr_answer, sep= ', ', end = '')
-print('>', end = '')
+# 3층 1 5 15 35
+# 2층 1 4 10 20
+# 1층 1 3 6 10
+# 0층 1 2 3 4
