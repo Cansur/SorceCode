f9dbe6b36204ee58c11caaf0f9b0a1c67b537451
Cansur <tjswo0682@gmail.com>
2023-03-01 15:32:39 +0900

    [1874] 실버2_스택수열

--- test/test.py
+++ test/test.py
@@ -1,17 +1,26 @@
-for _ in range(int(input())):
-    k = int(input())
-    n = int(input())
-    arr_inc = [i for i in range(1, n+1)]
-    arr_get = [0 for _ in range(n)]
-    for i in range(k-1):
-        for j in range(n):
-            for p in range(j+1):
-                arr_get[j] += arr_inc[p]
-        arr_inc = arr_get
-        arr_get = [0 for _ in range(n)]
-    print(sum(arr_inc))
+n = int(input())
+arr_n = []
+for _ in range(n):
+    arr_n.append(int(input()))
+arr_nn = [i for i in range(0, n+1)]
+index = 0
+arr_answer = []
 
-# 3층 1 5 15 35
-# 2층 1 4 10 20
-# 1층 1 3 6 10
-# 0층 1 2 3 4
+while True:
+    if arr_n[0] == arr_nn[index]:
+        arr_nn.pop(index)
+        arr_n.pop(0)
+        index -= 1
+        arr_answer.append('-')
+    elif arr_n[0] > arr_nn[index]:
+        index += 1
+        arr_answer.append('+')
+    elif arr_n[0] < arr_nn[index]:
+        arr_answer = []
+        break
+    if len(arr_nn) == 1:
+        break
+if len(arr_answer) == 0:
+    print('NO')
+else:
+    print(*arr_answer)
