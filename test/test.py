78d449c5a40f837d06214b05143290c15b637d2f
Cansur <tjswo0682@gmail.com>
2023-03-03 08:58:55 +0900

    [2805] 실버2_이분탐색

--- test/test.py
+++ test/test.py
@@ -1,26 +1,17 @@
-n = int(input())
-arr_n = []
-for _ in range(n):
-    arr_n.append(int(input()))
-arr_nn = [i for i in range(0, n+1)]
-index = 0
-arr_answer = []
+N, M = map(int, input().split())
+arr_tree = list(map(int, input().split()))
+start, end = 1, max(arr_tree)
 
-while True:
-    if arr_n[0] == arr_nn[index]:
-        arr_nn.pop(index)
-        arr_n.pop(0)
-        index -= 1
-        arr_answer.append('-')
-    elif arr_n[0] > arr_nn[index]:
-        index += 1
-        arr_answer.append('+')
-    elif arr_n[0] < arr_nn[index]:
-        arr_answer = []
-        break
-    if len(arr_nn) == 1:
-        break
-if len(arr_answer) == 0:
-    print('NO')
-else:
-    print(*arr_answer)
+while start <= end:
+    mid = (start+end) // 2
+    
+    length = 0 
+    for i in arr_tree:
+        if i >= mid:
+            length += i - mid
+    
+    if length >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
