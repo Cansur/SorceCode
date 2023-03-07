2c7860593ab49023608da82c5575e05a9f83ae81
Cansur <tjswo0682@gmail.com>
2023-03-07 19:57:57 +0900

    [7568] 실버5_덩치

--- test/test.py
+++ test/test.py
@@ -1,19 +1,13 @@
 N = int(input())
-cnt = N//5
+arr = []
 
-while True:
-    if (N-(cnt*5))%3 == 0:
-        cnt += (N-(cnt*5))/3
-        if cnt != int(cnt):
-            print(-1)
-            break
-        print(int(cnt))
-        break
-    elif cnt <= 0:
-        print(-1)
-        break
-    else:
-        cnt -= 1
+for _ in range(N):
+    x, y = map(int, input().split())
+    arr.append((x, y))
 
-
-
+for i in arr:
+    rank = 1
+    for j in arr:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = ' ')
