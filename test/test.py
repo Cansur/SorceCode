029e690527ba7d021f4527721023d6c221decd46
Cansur <tjswo0682@gmail.com>
2023-03-08 14:52:36 +0900

    [18870] 실버2

--- test/test.py
+++ test/test.py
@@ -1,4 +1,7 @@
-N, k = map(int, input().split())
-x = list(map(int, input().split()))
-x.sort(reverse=True)
-print(x[k-1])
+import sys
+N = int(input())
+X = list(map(int, sys.stdin.readline().split()))
+ranks = sorted(list(set(X)))
+dict = {ranks[i] : i for i in range(len(ranks))}
+for i in X:
+    print(dict[i], end = ' ')
