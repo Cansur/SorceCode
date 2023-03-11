bcfc5c1072cdf957a36251a058a8983b3a4ae95b
Cansur <tjswo0682@gmail.com>
2023-03-11 12:30:52 +0900

    [9506] 브론즈1

--- test/test.py
+++ test/test.py
@@ -1,7 +1,16 @@
-K = int(input())
-arr = []
-for _ in range(K):
-    var = int(input())
-    if var == 0: arr.pop()
-    else: arr.append(var)
-print(sum(arr))
+import sys
+while 1:
+    N = int(sys.stdin.readline())
+    arr = []
+    if N == -1: break
+
+    for i in range(1, N):
+        if N % i == 0: arr.append(i)
+    
+    if sum(arr) == N:
+        print(f'{N} = 1', end = '')
+        for i in range(1, len(arr)):
+            print(f' + {arr[i]}', end = '')
+        print()
+    else:
+        print(f'{N} is NOT perfect.')
