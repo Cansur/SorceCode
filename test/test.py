cb2fa626caa32e1b278edbd60e74b9d6b1801a81
Cansur <tjswo0682@gmail.com>
2023-03-25 11:54:46 +0900

    [4949] 실버4

--- test/test.py
+++ test/test.py
@@ -1,7 +1,29 @@
-arr_x, arr_y = [], []
-for _ in range(int(input())):
-    x, y = map(int, input().split())
-    arr_x.append(x)
-    arr_y.append(y)
-
-print((max(arr_x) - min(arr_x)) * (max(arr_y) - min(arr_y)))
+import sys
+while True:
+    str = sys.stdin.readline().rstrip()
+    flag = 0
+    stack = []
+    if str == '.':
+        break
+    for i in str:
+        if i == "(" or i == "[": #열린 괄호면 스택에 넣어준다.
+            stack.append(i)
+        elif i == ")": #닫는 소괄호가 등장했을 때
+            if not stack or stack[-1] == "[": #열린 괄호가 없거나 열린 대괄호가 나오면
+                print("no")
+                flag = 1
+                break
+            else:
+                stack.pop()
+        elif i == "]": # 닫는 대괄호가 등장했을 때
+            if not stack or stack[-1] == "(": #열린 괄호가 없거나 열린 소괄호가 나오면
+                print("no")
+                flag = 1
+                break
+            else:
+                stack.pop()
+    if flag == 0:   #앞서 no가 등장하지 않았을 때
+        if not stack :  #스택이 비어있다 = 모든 괄호가 짝을 맞췄다
+            print("yes")
+        else:
+            print("no")
