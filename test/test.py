3351048f1b1d8715dfb1ee13ee64deee20154b6d
Cansur <tjswo0682@gmail.com>
2023-03-27 18:09:08 +0900

    [1316] 실버5

--- test/test.py
+++ test/test.py
@@ -1,22 +1,12 @@
-import sys
-
-N, M, B = map(int, sys.stdin.readline().split())
-ground = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-answer = sys.maxsize
 idx = 0
-
-for target in range(257):
-    max_target, min_target = 0, 0
-
-    for i in range(N):
-        for j in range(M):
-            if ground[i][j] >= target:
-                max_target += ground[i][j] - target
-            else:
-                min_target += target - ground[i][j]
-
-    if max_target + B >= min_target:
-        if min_target + (max_target * 2) <= answer:
-            answer = min_target + (max_target * 2)
-            idx = target
-print(answer, idx)
+for _ in range(int(input())):
+    is_check = True
+    str = input()
+    for i in range(len(str)-1):
+        if str[i] != str[i+1]:
+            check_str = str[i+1:]
+            if check_str.count(str[i]) > 0:
+                is_check = False
+    if is_check:
+        idx += 1
+print(idx)
