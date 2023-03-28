6e4433e8f3fd212fbae8788609e8673832d9acd0
Cansur <tjswo0682@gmail.com>
2023-03-28 20:04:28 +0900

    [10798] 브론즈1

--- test/test.py
+++ test/test.py
@@ -1,12 +1,18 @@
-idx = 0
-for _ in range(int(input())):
-    is_check = True
+arr = [[0] * 15 for _ in range(5)]
+for idx in range(5):
     str = input()
-    for i in range(len(str)-1):
-        if str[i] != str[i+1]:
-            check_str = str[i+1:]
-            if check_str.count(str[i]) > 0:
-                is_check = False
-    if is_check:
-        idx += 1
-print(idx)
+    for i in range(len(str)):
+        arr[idx][i] = str[i]
+str = ''
+for i in range(15):
+    for j in range(5):
+        if arr[j][i] == 0: continue
+        str += arr[j][i]
+print(str)
+     
+# arr = ['WS']
+
+# try:
+#     print(arr[0][2])
+# except IndexError:
+#     print('')
