5bee0acc0b6ff9da1d66802ab65f18a7f8b7e8e2
Cansur <tjswo0682@gmail.com>
2023-04-07 11:47:30 +0900

    [1251] 실버5

--- test/test.py
+++ test/test.py
@@ -1,12 +1,11 @@
-dic = {}; cnt = 0
-for _ in range(int(input())):
-    str = input()
-    if str == 'ENTER':
-        dic = {}
-        continue
-    if dic.get(str):
-        continue
-    else:
-        dic[str] = 1
-        cnt += 1
-print(cnt)
+s = input()
+li = []
+for i in range(len(s)-2):
+    for j in range(i+1, len(s)-1):
+        for k in range(j+1, len(s)):
+            s1 = s[:j][::-1]
+            s2 = s[j:k][::-1] 
+            s3 = s[k:][::-1]
+            t = s1 + s2 + s3
+            li.append(t)
+print(min(li))
