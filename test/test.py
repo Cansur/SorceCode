93fd78e063e6d644dc3b85128d60dbced3aef482
Cansur <tjswo0682@gmail.com>
2023-04-08 19:18:38 +0900

    [1267] 브론즈3

--- test/test.py
+++ test/test.py
@@ -1,11 +1,12 @@
-s = input()
-li = []
-for i in range(len(s)-2):
-    for j in range(i+1, len(s)-1):
-        for k in range(j+1, len(s)):
-            s1 = s[:j][::-1]
-            s2 = s[j:k][::-1] 
-            s3 = s[k:][::-1]
-            t = s1 + s2 + s3
-            li.append(t)
-print(min(li))
+N = int(input())
+x = list(map(int, input().split()))
+a, b = 0, 0
+for i in x:
+    a += (i // 30) + 1
+    b += (i // 60) + 1
+a *= 10; b *= 15
+if a < b: print('Y', a)
+if a > b: print('M', b)
+if a == b: print('Y M', b)
+# 71초를 사용
+# 영식은 30초 마다 계산 : 61 + 10 = 30 + 10
