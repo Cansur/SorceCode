a8bc07a63ea71a53a3dc6f49025e54ef5e72935f
Cansur <tjswo0682@gmail.com>
2023-04-09 23:13:56 +0900

    [1284] 브론즈3

--- test/test.py
+++ test/test.py
@@ -1,12 +1,19 @@
-N = int(input())
-x = list(map(int, input().split()))
-a, b = 0, 0
-for i in x:
-    a += (i // 30) + 1
-    b += (i // 60) + 1
-a *= 10; b *= 15
-if a < b: print('Y', a)
-if a > b: print('M', b)
-if a == b: print('Y M', b)
-# 71초를 사용
-# 영식은 30초 마다 계산 : 61 + 10 = 30 + 10
+dic = {
+    '1' : 2,
+    '2' : 3,
+    '3' : 3,
+    '4' : 3,
+    '5' : 3,
+    '6' : 3,
+    '7' : 3,
+    '8' : 3,
+    '9' : 3,
+    '0' : 4
+}
+while 1:
+    N = input()
+    if N == '0': break
+    length = len(N)+1
+    for i in range(len(N)):
+        length += dic[N[i]]
+    print(length)
