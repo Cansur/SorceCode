e2ddb15c454260235195217ba4f299f4a835104d
Cansur <tjswo0682@gmail.com>
2023-04-10 22:29:01 +0900

    [1547] 브론즈3

--- test/test.py
+++ test/test.py
@@ -1,19 +1,9 @@
-dic = {
-    '1' : 2,
-    '2' : 3,
-    '3' : 3,
-    '4' : 3,
-    '5' : 3,
-    '6' : 3,
-    '7' : 3,
-    '8' : 3,
-    '9' : 3,
-    '0' : 4
-}
-while 1:
-    N = input()
-    if N == '0': break
-    length = len(N)+1
-    for i in range(len(N)):
-        length += dic[N[i]]
-    print(length)
+M = int(input())
+answer = 1
+for _ in range(M):
+    x, y = map(int, input().split())
+    if answer == x:
+        answer = y
+    elif answer == y:
+        answer = x
+print(answer)
