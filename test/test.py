d0224bbfc0a781a464909a5395094d516b7aaf88
Cansur <tjswo0682@gmail.com>
2023-04-14 10:25:39 +0900

    [1308] 실버5_D-Day

--- test/test.py
+++ test/test.py
@@ -1,9 +1,9 @@
-M = int(input())
-answer = 1
-for _ in range(M):
-    x, y = map(int, input().split())
-    if answer == x:
-        answer = y
-    elif answer == y:
-        answer = x
-print(answer)
+import datetime
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+date1 = datetime.datetime(a[0],a[1],a[2])
+date2 = datetime.datetime(b[0],b[1],b[2])
+if (date2 - date1).days >= 365243:
+    print('gg')
+    exit()
+print(f'D-{(date2-date1).days}')
