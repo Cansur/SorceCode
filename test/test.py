1aead58002f30495aa2c85d6a992ea22283a6cb0
Cansur <tjswo0682@gmail.com>
2023-04-15 14:05:49 +0900

    [1312] 실버5_소수

--- test/test.py
+++ test/test.py
@@ -1,9 +1,7 @@
-import datetime
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-date1 = datetime.datetime(a[0],a[1],a[2])
-date2 = datetime.datetime(b[0],b[1],b[2])
-if (date2 - date1).days >= 365243:
-    print('gg')
-    exit()
-print(f'D-{(date2-date1).days}')
+A, B, N = map(int, input().split())
+for i in range(N):
+    A = (A%B)*10
+    d = A%B
+    num = A//B
+
+print(num)
